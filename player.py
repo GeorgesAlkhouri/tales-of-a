718a49859ecf3095f0d3a5cacc918664431e1b6e
ibirothe <132349052+ibirothe@users.noreply.github.com>
2023-05-06 18:41:55 +0200

    Added emojis and stat functions player.py
    Demon4 and Chest in icons
    
    get_traits, max_weapon_durability

--- player.py
+++ player.py
@@ -38,16 +38,28 @@ icons = {
     'Chop': '<:Chop:1099424386546683944>',
     'Club': '<:Club:1096410443037278281>',
     'Defense': '<:Defense:1097394054070808717>',
-    'Demon': '<:Demon:1096696395697754182>'
+    'Demon': '<:Demon:1096696395697754182>',
+    'Demon4': '<:Demon4:1104429614836764792>',
+    'Chest': '<:Chest:1104429086601908234>'
 }
 
 
 
 class Player:
-    def __init__(self, health, energy):
+    def __init__(self, health, energy, strength, dexterity, defense):
         self.health = health
+        self.max_health = health
         self.energy = energy
-        self.inventory = ["Sword", "Shield", "Potion"]
+        self.max_energy = energy
+        self.strength = strength
+        self.dexterity = dexterity
+        self.defense = defense
+        self.world_stage = 1
+        self.inventory = {"Wood": 3, "Stone": 5, "Iron": 6, "Water": 6, "Herbs": 2}
+        self.weapon = "Bow"
+        self.max_weapon_durability = 10
+        self.armor = ""
+        self.traits = {"Lumberjack": 1, "Druid": 1, "Wanderer": 1}
 
     def decrease_energy(self):
         self.energy -= 1
@@ -59,18 +71,64 @@ class Player:
         self.health -= 1
         if self.health <= 0:
             self.health = 0
+    
+    def regen_health(self):
+        self_health = self.max_health
+    
+    def regen_energy(self):
+        self_energy = self.max_energy
 
     def get_status(self):
         print(f"Health: {self.health} | Energy: {self.energy}")
-        return f"{icons['health']} Health: {self.health} {icons['energy']} Energy: {self.energy}"
+        return f"**{self.health}/{self.max_health}** {icons['health']} Health /n**{self.energy}/{self.max_energy}** {icons['energy']} Energy"
+    
 
     def get_inventory(self):
         if not self.inventory:
-            return "Your inventory is empty."
+            inventory_list = "Your inventory is empty."
         else:
             inventory_list = ""
-            for item in self.inventory:
+            for key, value in self.inventory.items():
                               
-                inventory_list += f"{icons[item]} • `{item}` • **4** \n"
+                inventory_list += f"{icons[key]} • `{key}` • **{value}** \n"
         
         return inventory_list
+    
+    def get_weapon(self):
+        if not self.inventory:
+            weapon_list = "You are unarmed."
+        else:
+            item = self.weapon
+            max_weapon_durability = self.max_weapon_durability
+            weapon_list = f"{icons[item]} • `{item}` • **10/{max_weapon_durability}** \n"
+        
+        return weapon_list
+    
+    def get_attributes(self):
+        strength = self.strength
+        defense = self.defense
+        dexterity = self.dexterity
+        world_stage = self.world_stage
+        attributes_list = f"{icons['Strength']} • `Strength` • **{strength}** \n{icons['Defense']} • `Defense` • **{defense}** \n{icons['Dexterity']} • `Dexterity` • **{dexterity}** \n{icons['WorldStage']} • `World Stage` • **{world_stage}** \n"
+        
+        return attributes_list
+    
+    def get_constitution(self):
+        health = self.health
+        energy = self.energy
+        max_health = self.max_health
+        max_energy = self.max_energy
+        constitution_list = f"{icons['Health']} • `Health` • **{health}/{max_health}** \n{icons['Energy']} • `Energy` • **{energy}/{max_energy}**"
+        
+        return constitution_list
+        
+    def get_traits(self):
+        if not self.traits:
+            traits_list = "You got no active traits."
+        else:
+            trait_list = ""
+            for key, value in self.traits.items():
+                              
+                traits_list += f"{icons["Buff"]} • `{key}` • **{value}** \n"
+        
+        return traits_list
